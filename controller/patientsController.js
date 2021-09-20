29fedbaf9f23643eb7e1a2af8d108ec6d8faa6e2
Abhishek Verma <avishekv444@gmail.com>
2021-09-21 01:28:42 +0530

    return all reports of patient

--- controller/patientsController.js
+++ controller/patientsController.js
@@ -69,3 +69,32 @@ module.exports.createReport=async function(req,res){
     }
 }
 
+
+module.exports.allReports=async function(req,res){
+    try {
+        const doctorJWTToken = req.headers.authorization;
+        const token = doctorJWTToken.split(' ');
+        const decoded = jwt.verify(token[1], 'Abhishek99HospitalAPI');
+        const doctor=await Doctor.findById(decoded._id);
+        if(!doctor){
+            return res.json('401',{
+                message:"Doctor does not Exist"
+            });
+        }
+        let patient=await Patient.findById(req.params.id);
+        if(!patient){
+            return res.json(404,{
+                message:'Patient needs to register first as this Patient does not exist',
+            });
+        }
+        const reportArrays=patient.repots.sort({date:-1});
+        return res.json(200,{
+            message:`List of all ${patient.name} reports are:` ,
+            reportArrays
+        })
+    } catch (error) {
+        return res.json(500,{
+            message:'Internal Server Error'
+        });
+    }
+}
--- routes/patient.js
+++ routes/patient.js
@@ -9,4 +9,6 @@ router.post('/register',passport.authenticate('jwt',{ session:false }),patientCo
 
 router.post('/:id/create_report',passport.authenticate('jwt',{ session:false }),patientController.createReport);
 
+router.get('/:id/all_reports',passport.authenticate('jwt',{ session:false }),patientController.allReports);
+
 module.exports=router;
