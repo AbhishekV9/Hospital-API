6f3a041e784400eaf51eb71069dbff2030ce518e
Abhishek Verma <avishekv444@gmail.com>
2021-09-21 23:53:52 +0530

    From localStorage to cloudStorsge

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,14 @@
 const mongoose=require('mongoose');
+const uri = process.env.HospitalApi_URI || 'mongodb+srv://avishekv444:hppavalion@hospitalapi.ebv8r.mongodb.net/HospitalApiDB?retryWrites=true&w=majority'
 
-mongoose.connect('mongodb://localhost/Hospital_API');
+mongoose
+  .connect(uri, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to Atlas database ');
+  })
+  .catch((err) => {
+    console.error(`Error connecting to the database. \n${err}`);
+  });
 
 const db=mongoose.connection;
 
